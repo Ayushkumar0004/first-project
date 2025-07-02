0524efcc9f11bc57c457f4ed161f2426a212dba2
Ayush <Ayushk0004@gmail.com>
2025-07-02 21:49:14 +0530

    React task2

---
+++ my-task2/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [input, setInput] = useState("")
+  const [todo, setTodo] = useState([])
+
+  const handleAdd = () => {
+    if (input.trim() === "") return
+    setTodo([...todo, input])
+    setInput("")
+  }
+
+  return (
+    <div className='app'>
+      <div className='todo-card'>
+        <h1>📝Todo List</h1>
+        <div className='input-group'>
+          <input
+            placeholder="What do you need to do?"
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
+            onKeyDown={(event) => {
+              if (event.key === 'Enter') {
+                handleAdd()
+              }
+            }}
+          />
+          <button onClick={handleAdd}>+ Add</button>
+        </div>
+
+        <div className='todo-list'>
+          {todo.map((item, index) => (
+            <p key={index} className='todo-item'>{item}</p>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
